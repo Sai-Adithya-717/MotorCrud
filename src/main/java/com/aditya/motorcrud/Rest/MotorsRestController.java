32a287eec459ddb6f885aa1dc51f80ae89618aba
WideCarSupporter <gvsaditya1995@gmail.com>
2023-10-05 06:41:13 +0530

    changes commit

--- src/main/java/com/aditya/motorcrud/Rest/MotorsRestController.java
+++ src/main/java/com/aditya/motorcrud/Rest/MotorsRestController.java
@@ -20,7 +20,7 @@ public class MotorsRestController {
         motorDao = theMotorDao;
     }
 
-    //explore "/motors" and return a list
+    //explore "/motors" and return a list of motors
     @GetMapping("Motors")
     public List<Motors> findAll(){
      return motorDao.findAll();
