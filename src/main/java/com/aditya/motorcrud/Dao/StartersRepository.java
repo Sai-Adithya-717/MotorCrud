b0b583f5573427f51cd2aa2b069db25798738a26
WideCarSupporter <gvsaditya1995@gmail.com>
2023-10-20 21:57:19 +0530

    removed repository

--- src/main/java/com/aditya/motorcrud/Dao/StartersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aditya.motorcrud.Dao;
-
-import com.aditya.motorcrud.Entity.Motors;
-import com.aditya.motorcrud.Entity.Starters;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StartersRepository extends JpaRepository<Motors,Integer> {
-}
